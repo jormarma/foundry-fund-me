439cfea45d271fb5c5f0016a851d2b7d98b8d3c7
Jorge Martí Marín <jormarma@gmail.com>
2025-10-25 13:06:57 +0200

    Add FundMeTest

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+
+contract FundMeTest is Test {
+    function setUp() external {}
+
+    function testDemo() public {}
+}
