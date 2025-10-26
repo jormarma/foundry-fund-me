643b7481c63649283b5e77b07d82ee0790390eb0
Jorge Martí Marín <jormarma@users.noreply.github.com>
2025-10-26 02:34:26 +0200

    04/add testing (#4)
    * Add FundMeTest
    
    * Refactor previous documentation
    
    * Add documentation for this step
    
    * Add a test checking the owner of the contract

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
@@ -9,10 +8,15 @@ contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
+        // The FundMeTest contract is deploying the contract in our local blockchain
         fundMe = new FundMe();
     }
 
     function testFundMeMinimumDollarIsFive() public view {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), address(this));
     }
 }
